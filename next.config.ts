719e0fd62611ea4185f495d1559f439fd9311324
dev-anthony <anthonyachibi@gmail.com>
2026-06-11 11:42:44 -0700

    updt

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Force Webpack to ignore Node.js server-only modules when bundling code for the browser
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
